f09b37f6a2ea50dc80947906ecbc06df370900b1
Frank Cardoso <frank.ctba@gmail.com>
2022-04-05 22:15:32 -0300

    Added new item

--- HelloWorld.Api/Controllers/WeatherForecastController.cs
+++ HelloWorld.Api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,8 @@ namespace HelloWorld.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+            "Congelante"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
